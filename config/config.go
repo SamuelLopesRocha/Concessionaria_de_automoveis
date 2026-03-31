e2c5b1682f1df846b642c75b6123c2dd2f68e959
genari05 <tgenari791@gmail.com>
2026-03-31 16:20:38 -0300

    s

--- config/config.go
+++ config/config.go
@@ -1,28 +1,28 @@
 package config
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-import "os"
-
 func ConnectDatabase() {
-   dsn := os.Getenv("DATABASE_URL")
-   if dsn == "" {
-	   panic("❌ Variável de ambiente DATABASE_URL não definida!")
-   }
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		panic("❌ Variável de ambiente DATABASE_URL não definida!")
+	}
 
-   database, err := gorm.Open(postgres.New(postgres.Config{
-	   DSN:                  dsn,
-	   PreferSimpleProtocol: true, // 🔴 resolve o bug do stmtcache
-   }), &gorm.Config{})
+	database, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true, // resolve bug stmtcache
+	}), &gorm.Config{})
 
-   if err != nil {
-	   panic("❌ Falha ao conectar ao PostgreSQL do Supabase")
-   }
+	if err != nil {
+		panic("❌ Falha ao conectar ao PostgreSQL do Supabase")
+	}
 
-   DB = database
+	DB = database
 }
